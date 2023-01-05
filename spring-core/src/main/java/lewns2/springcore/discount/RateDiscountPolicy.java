1f4a0e529b9be11ccd276ec3c0b3954412ec0948
lewns2 <lewns2002@naver.com>
2023-01-05 15:10:46 +0900

    [add] 새로운 할인 정책 구현 및 변경 시 문제점 발생

---
+++ spring-core/src/main/java/lewns2/springcore/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package lewns2.springcore.discount;
+
+import lewns2.springcore.member.Grade;
+import lewns2.springcore.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
--- spring-core/src/main/java/lewns2/springcore/order/OrderServiceImpl.java
+++ spring-core/src/main/java/lewns2/springcore/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package lewns2.springcore.order;
 
 import lewns2.springcore.discount.DiscountPolicy;
 import lewns2.springcore.discount.FixDiscountPolicy;
+import lewns2.springcore.discount.RateDiscountPolicy;
 import lewns2.springcore.member.Member;
 import lewns2.springcore.member.MemberRepository;
 import lewns2.springcore.member.MemoryMemberRepository;
@@ -12,7 +13,8 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     // 할인 정책 찾기
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // DIP 위반 : 추상(인터페이스)에만 의존하지 않고 구체(구현)에도 의존한다. OCP 위반 : 할인 정책 변경(기능 변경)에 따른 "OrderServieImpl" 코드 변경(클라이언트 코드에 영향)을 하므로
+    private final DiscountPolicy discountPolicy; // DIP 해결, 다만 구현체가 없어 누군가 대신 생성하고 주입해야한다. => 이를 위해 공연 기획자(AppConfig)가 필요하다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
