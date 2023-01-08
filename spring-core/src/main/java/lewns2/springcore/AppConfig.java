874dbfdacb935c40564b311dd6689d8b31261bd6
lewns2 <lewns2002@naver.com>
2023-01-08 21:28:57 +0900

    [add] 스프링 컨테이너 사용하기

--- spring-core/src/main/java/lewns2/springcore/AppConfig.java
+++ spring-core/src/main/java/lewns2/springcore/AppConfig.java
@@ -9,21 +9,28 @@ import lewns2.springcore.member.MemberServiceImpl;
 import lewns2.springcore.member.MemoryMemberRepository;
 import lewns2.springcore.order.OrderService;
 import lewns2.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
